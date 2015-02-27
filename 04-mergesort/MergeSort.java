43099df5f3292183f1928b8a17d2429c4dcc2f59
RyanTu <tu8.rya@gmail.com>
2015-02-27 08:41:31 -0500

    Still has errors splitting

--- 04-mergesort/MergeSort.java
+++ 04-mergesort/MergeSort.java
@@ -3,7 +3,6 @@ import java.util.*;
 public class MergeSort{
 
     ArrayList<Integer> list = new ArrayList<Integer>();
-    ArrayList<Integer> blank = new ArrayList<Integer>();
 
     public ArrayList<Integer> merge(ArrayList<Integer> a, ArrayList<Integer> b){
 	ArrayList<Integer> data = new ArrayList<Integer>();
@@ -21,45 +20,51 @@ public class MergeSort{
 		data.add(a.remove(0));
 	    }
 	}
+	//System.out.println(data);
 	return data;
     }
 
-    public ArrayList<Integer> half(ArrayList<Integer> data){
-	ArrayList<Integer> half = new ArrayList<Integer>();
-	int cut = data.size()/2;
-	for (int i = 0; i<cut; i++){
-	    half.add(data.remove(0));
+    public ArrayList<Integer> mergeSort(ArrayList<Integer> stuff){
+	ArrayList<Integer> ans = new ArrayList<Integer>();
+	ArrayList<Integer> halfA = new ArrayList<Integer>();
+	ArrayList<Integer> halfB = new ArrayList<Integer>();
+	int restSize = stuff.size() - stuff.size()/2;
+	if (stuff.size() <= 1){
+	    return stuff;
 	}
-	return half;
-    }
-
-    public ArrayList<Integer> mergeSort(ArrayList<Integer> data, boolean split){
-	ArrayList<Integer> result = new ArrayList<Integer>();
-	if (data.size() <= 1){
-	    merge(data,blank);
-	} else if (!split) {
-	    mergeSort(half(data, false));
-	    mergeSort(data, false);
-	} else {
-	    
+	else {
+	    //System.out.println(stuff.size());
+	    for (int i = 0; i<stuff.size()/2; i++){
+		halfA.add(stuff.remove(0));
+		//System.out.print(halfA);
+		//System.out.println(stuff);
+	    }
+	    System.out.println(stuff.size());
+	    for (int i = 0; i<restSize; i++){
+		halfB.add(stuff.remove(0));
+		//System.out.print(halfB);
+		//System.out.println(stuff);
+	    }
+	    ans = merge(mergeSort(halfA),mergeSort(halfB));
 	}
-	return result;
+	return ans;
     }
 
     public static void main(String[] args){
+	MergeSort m = new MergeSort();
+	Random r = new Random();
 	ArrayList<Integer> data = new ArrayList<Integer>();
 	//ArrayList<Integer> set1 = new ArrayList<Integer>();
 	//ArrayList<Integer> set2 = new ArrayList<Integer>();
-	for (int i = 0; i<20; i++){
+	for (int i = 0; i<8; i++){
 	    data.add(r.nextInt(30));
 	    //set1.add(r.nextInt(10) + (i*10));
 	    //set2.add(r.nextInt(10) + (i*10));
 	}
-	MergeSort m = new MergeSort();
-	Random r = new Random();
 	//System.out.println(set1);
 	//System.out.println(set2);
 	//System.out.println(m.merge(set1,set2));
+	System.out.println(data);
 	System.out.println(m.mergeSort(data));
     }
 
