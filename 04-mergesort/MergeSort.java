38385eb3bfcb0ee6ad1c9afb42582e3bad16a509
RyanTu <tu8.rya@gmail.com>
2015-03-01 21:10:06 -0500

    MergeSort for ArrayList

--- 04-mergesort/MergeSort.java
+++ 04-mergesort/MergeSort.java
@@ -25,45 +25,30 @@ public class MergeSort{
     }
 
     public ArrayList<Integer> mergeSort(ArrayList<Integer> stuff){
-	ArrayList<Integer> ans = new ArrayList<Integer>();
 	ArrayList<Integer> halfA = new ArrayList<Integer>();
 	ArrayList<Integer> halfB = new ArrayList<Integer>();
-	int restSize = stuff.size() - stuff.size()/2;
 	if (stuff.size() <= 1){
 	    return stuff;
 	}
 	else {
-	    //System.out.println(stuff.size());
-	    for (int i = 0; i<stuff.size()/2; i++){
+	    int len = stuff.size();
+	    for (int i = 0; i<len/2; i++){
 		halfA.add(stuff.remove(0));
-		//System.out.print(halfA);
-		//System.out.println(stuff);
 	    }
-	    System.out.println(stuff.size());
-	    for (int i = 0; i<restSize; i++){
+	    for (int i = 0; i<stuff.size(); ){
 		halfB.add(stuff.remove(0));
-		//System.out.print(halfB);
-		//System.out.println(stuff);
 	    }
-	    ans = merge(mergeSort(halfA),mergeSort(halfB));
 	}
-	return ans;
+	return merge(mergeSort(halfA),mergeSort(halfB));
     }
 
     public static void main(String[] args){
 	MergeSort m = new MergeSort();
 	Random r = new Random();
 	ArrayList<Integer> data = new ArrayList<Integer>();
-	//ArrayList<Integer> set1 = new ArrayList<Integer>();
-	//ArrayList<Integer> set2 = new ArrayList<Integer>();
-	for (int i = 0; i<8; i++){
+	for (int i = 0; i<20; i++){
 	    data.add(r.nextInt(30));
-	    //set1.add(r.nextInt(10) + (i*10));
-	    //set2.add(r.nextInt(10) + (i*10));
 	}
-	//System.out.println(set1);
-	//System.out.println(set2);
-	//System.out.println(m.merge(set1,set2));
 	System.out.println(data);
 	System.out.println(m.mergeSort(data));
     }
