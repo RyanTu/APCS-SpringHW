2c73136571c96d1c0d1e217733ee1469abc51c29
RyanTu <tu8.rya@gmail.com>
2015-03-08 13:40:37 -0400

    Recursive select

--- 05-selection/Selection.java
+++ 05-selection/Selection.java
@@ -1,35 +1,45 @@
 public class Selection{
 
-    public int[] partition(int[] L, int start, int end){
-	int[] D = new int[end-start+1];
-	int pivot = L[start];
-	int startD = 0;
-	int endD = end-start;
-        int i = start;
-	while(startD != endD){
-	    if (L[i] == pivot){
+    public int select(int[] A, int k, int l, int h){
+	int lowIndex = l;
+	int highIndex = h-1;
+	int pivot = A[l];
+	int pivotIndex = 0;
+	A[l] = A[h];
+	A[h] = pivot;
+        while (lowIndex < highIndex){
+	    if (A[lowIndex] < pivot){
+		lowIndex++;
 	    }
-	    else if (L[i]<pivot){
-		D[startD] = L[i];
-		startD++;
+	    else {
+		int temp = A[lowIndex];
+		A[lowIndex] = A[highIndex];
+		A[highIndex] = temp;
+		highIndex--;
 	    }
-	    else{
-		D[endD] = L[i];
-		endD--;
-	    }
-	    i++;
 	}
-	D[startD]=pivot;
-	return D;
+	if (A[lowIndex] < pivot){
+	    A[h] = A[lowIndex+1];
+	    A[lowIndex+1] = pivot;
+	    pivotIndex = lowIndex+1;
+	}
+	else {
+	    A[h] = A[lowIndex];
+	    A[lowIndex] = pivot;
+	    pivotIndex = lowIndex;
+	}
+	if (pivotIndex > k){
+	    select(A,k,l,pivotIndex-1);
+	}
+        if (pivotIndex < k){
+	    select(A,k,pivotIndex+1,h);
+	}
+	return A[k];
     }
 
     public static void main(String[] args){
 	Selection s = new Selection();
-	int[] data = {5,2,43,27,3,12,15,7,14,23,45,37,25,34,32,24};
-	int[] ans = s.partition(data,0,data.length-1);
-	for (int i = 0; i<ans.length; i++){
-	    System.out.print(" " + ans[i]);
-	}
-	System.out.println();
+	int[] data = {4,7,2,9,3,1,2,1,5,6,8};
+        System.out.println(s.select(data,3,0,data.length-1));
     }
 }
