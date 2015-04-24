894de5e67c656e283d3d44c23e02a7fe221d14a9
RyanTu <tu8.rya@gmail.com>
2015-04-24 06:50:11 -0400

    Added methods, untested

--- 14-Tree/Tree.java
+++ 14-Tree/Tree.java
@@ -2,7 +2,7 @@ public class Tree{
 
     Node tree = new Node();
 
-    public Node search(Node t, Integer i){
+    public Node search(Node t, int i){
 	while (t != null){
 	    int c = t.getData().compareTo(i);
 	    if (c>0)
@@ -15,6 +15,25 @@ public class Tree{
 	return null;
     }
 
-    public void insert(Node n)
+    public void insert(Node t, int i){
+	Node n = new Node(i);
+	if (t == null){
+	    t = n;
+	}
+	Node lead = t;
+	Node follow = null;
+	while (lead != null){
+	    follow = lead;
+	    int c = i.compareTo(lead.getData());
+	    if (c<0)
+		lead = lead.getLeft();
+	    else
+		lead = lead.getRight();
+	}
+	if (i.compareTo(follow.getData())<0)
+	    follow.setLeft(n);
+	else
+	    follow.setRight(n);
+    }
     
 }
