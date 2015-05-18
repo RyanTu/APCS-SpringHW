b7345e042aa58a2dbdc1346bdf4275aa35f46d0b
RyanTu <tu8.rya@gmail.com>
2015-05-18 02:06:07 -0400

    It looks like it works...

--- 16-Heap/Heap.java
+++ 16-Heap/Heap.java
@@ -3,39 +3,83 @@ import java.util.*;
 public class Heap{
 
     private ArrayList<Integer> heap;
-    private int root;
-    private int last;
 
     public Heap(int t){
 	heap = new ArrayList<Integer>();
 	heap.add(t);
-	root = heap.get(0);
-	last = 0;
     }
 
-    public void insert(int i){
-	heap.add(i);
-	last++;
-	int index = last;
-	while(index-1 >= 0 && heap.get((index-1)/2) > i){ 
+    public int removeMin(){
+	heap.set(0,heap.get(heap.size()-1));
+	int ret = heap.remove(heap.size()-1);
+	pushDown(0);
+	return ret;
+    }
+
+    public void insert(int n){
+	heap.add(n);
+	siftUp(heap.size()-1);
+    }
+
+    public void pushDown(int i){
+	int value = heap.get(i);
+	int index = i;
+	int leftIndex = ((i+1)*2)-1;
+	int rightIndex = (i+1)*2;
+	while ((heap.size()>(index+1)*2)&&(heap.get(i)>heap.get(leftIndex) || heap.get(i)>heap.get(rightIndex))){
+	    if (heap.get(leftIndex) < heap.get(rightIndex)){
+		heap.set(index,heap.get(leftIndex));
+		heap.set(leftIndex,value);
+		index = leftIndex;
+	    }
+	    else {
+		heap.set(index,heap.get(rightIndex));
+		heap.set(rightIndex,value);
+		index = rightIndex;
+	    }
+	    leftIndex = ((i+1)*2)-1;
+	    rightIndex = (i+1)*2;
+	}
+    }
+
+    public void siftUp(int i){
+	int value = heap.get(i);
+	int index = i;
+	while(index-1 >= 0 && heap.get((index-1)/2) > value){ 
 	    int temp = heap.get((index-1)/2);
-	    heap.set((index-1/2),i);
-	    heap.set(last,temp);
+	    heap.set((index-1)/2,value);
+	    heap.set(index,temp);
 	    index = (index-1)/2;
 	}
     }
 
-    public String toString(){
-	String s = "";
-	for (int i = 0; i<heap.size(); i++){
-	    s += heap.get(i) + " ";
+    public void heapSort(){
+	heapSort(heap);
+    }
+
+    public void heapSort(ArrayList<Integer> data){
+	for (int i = data.size()-1; i > 0; i--){
+	    pushDown(i);
 	}
-	return s;
+    }
+
+    public String toString(){
+	return heap + "";
     }
     
     public static void main(String[] args){
 	Heap h = new Heap(5);
+	h.insert(3);
+	h.insert(4);
 	h.insert(6);
+	h.insert(2);
+	h.insert(15);
+	h.insert(30);
+	h.insert(13);
+	System.out.println(h);
+	h.removeMin();
+	System.out.println(h);
+	h.heapSort();
 	System.out.println(h);
     }
     
